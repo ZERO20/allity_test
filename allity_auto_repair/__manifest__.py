ea96a5bf158a8640b34d0eb3baa79f7a447960bd
Edgar de la Cruz <edgargdcv@gmail.com>
2025-06-20 10:40:10 -0600

    [ADD] allity_auto_repair

---
+++ allity_auto_repair/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Allity Taller Mecánico",
+    "summary": "Gestión de reparaciones de vehículos para talleres mecánicos",
+    "description": """
+        Este módulo permite a Allity gestionar reparaciones, órdenes de trabajo, vehículos y clientes
+        dentro de un taller mecánico. Incluye funcionalidades personalizadas según los requerimientos del negocio.
+    """,
+    "author": "Edgar de la Cruz",
+    "support": "edgargdcv@gmail.com",
+    "website": "https://allity.com/",
+    "company": "Allity",
+    "maintainer": "Edgar de la Cruz",
+    'category': 'Industries',
+    "version": "18.0.0.1",
+    "depends": [
+        "base"
+    ],
+    "data": [],
+    "license": "OPL-1",
+    "installable": True,
+    "application": True,
+}
